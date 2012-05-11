e1aca6a38370a62ccd2c34da5d0c933eaccefa84
pete <p.reisinger@gmail.com>
2012-05-11 23:05:14 +0100

    set default position for player ship

--- dart-invaders/dart/PlayerShip.dart
+++ dart-invaders/dart/PlayerShip.dart
@@ -1,7 +1,20 @@
 
 class PlayerShip extends GameObject {
 
-  PlayerShip() : super(0, 0, 640, 480) {
-    
+  static final WIDTH = 50;
+  static final HEIGHT = 50;
+
+  PlayerShip() : super(0, 0, WIDTH, HEIGHT) {
+
+    // place player ship at the bottom in the middle
+    y = canvas.height - HEIGHT;
+    x = (canvas.width / 2) - (WIDTH / 2);
+
+    ctx.on.keyDown.add((Event e) {
+      // TODO - check what key was pressed
+      x++;
+      paint();
+    }, true);
   }
+
 }
