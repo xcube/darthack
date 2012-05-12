98a6b5f204de94864640f33e63c073287f495363
Dave Snowdon <dave@davesnowdon.com>
2012-05-12 09:59:31 +0100

    MovingObject now handles getting to the end of a path

--- dart-invaders/dart/MovingObject.dart
+++ dart-invaders/dart/MovingObject.dart
@@ -1,14 +1,22 @@
 class MovingObject extends GameObject {
 
+  static final int PATH_END_STOP = 1;
+  static final int PATH_END_REPEAT = 2;
+  static final int PATH_END_REMOVE = 3;
+
   Point delta; // speed in units/second
   List<Point> path;
   int pathIndex;
   double pathSpeed;
   double timeToNextPoint;
+  int pathEndBehaviour;
+  bool moving;
 
   MovingObject(GameContext gameContext, int x, int y, int width, int height, Point delta) : super(gameContext, x, y, width, height) {
     this.delta = new Point(delta.x, delta.y);
     this.path = null;
+    this.pathEndBehaviour = PATH_END_STOP;
+    this.moving = true;
   }
 
   MovingObject.path(GameContext gameContext, int width, int height, List<Point> wayPoints, double speed) {
@@ -18,27 +26,59 @@ class MovingObject extends GameObject {
     this.pos.x = path[0].x;
     this.pos.y = path[0].y;
     nextPathElement(path[1]);
+    this.pathEndBehaviour = PATH_END_STOP;
+    this.moving = true;
   }
 
   void tick(timeSinceLastTick) {
-    pos.x += timeSinceLastTick * delta.x;
-    pos.y += timeSinceLastTick * delta.y;
+    if (moving) {
+      pos.x += timeSinceLastTick * delta.x;
+      pos.y += timeSinceLastTick * delta.y;
 
-    if (null != path) {
-      timeToNextPoint -= timeSinceLastTick;
-      if (timeToNextPoint < 0) {
-        ++pathIndex;
-        nextPathElement(path[pathIndex]);
+      if (null != path) {
+        timeToNextPoint -= timeSinceLastTick;
+        if (timeToNextPoint < 0) {
+          ++pathIndex;
+          if (pathIndex >= path.length) {
+            pathEnd(); // handle end of path behavior
+            // if we are still moving, then loop to beginning of path
+            if (moving) {
+              nextPathElement(path[0]);
+            }
+          } else {
+            nextPathElement(path[pathIndex]);
+          }
+        }
       }
     }
   }
 
   void nextPathElement(Point step) {
+    if (!moving) {
+      return;
+    }
+
     double dx = pos.x - step.x;
     double dy = pos.y - step.y;
     double dist = Math.sqrt((dx*dx) + (dy*dy));
     timeToNextPoint = dist / pathSpeed;
     delta.x = dx / timeToNextPoint;
     delta.y = dy / timeToNextPoint;
+  }
+
+  /* called when object reaches the end of its path, sub-classes can override for more complec behaviour*/
+  void pathEnd() {
+    switch (pathEndBehaviour) {
+      case PATH_END_STOP:
+        moving = false;
+        break;
+      case PATH_END_REPEAT:
+        pathIndex = 0;
+        break;
+      case PATH_END_REMOVE:
+        this.moving = false;
+        remove();
+        break;
+    }
   }
 }
