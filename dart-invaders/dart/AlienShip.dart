19fddc22d16d4758dcb75280fce431ef77b408ce
Dave Snowdon <dave@davesnowdon.com>
2012-05-12 15:33:46 +0100

    added rate limiting to alien ships bombing

--- dart-invaders/dart/AlienShip.dart
+++ dart-invaders/dart/AlienShip.dart
@@ -1,6 +1,9 @@
 class AlienShip extends PathAnimatedImageObject {
 
+  static final int SHOOT_RATE_MILLIS = 1000;
+
   int pointsWorth;
+  int lastShoot = 0;
 
   AlienShip(GameContext gameContext, int width, int height, List<Point> wayPoints, double speed, String bitmap, int points) : super(gameContext, width, height, wayPoints, speed, bitmap) {
     this.pointsWorth = points;
@@ -9,13 +12,15 @@ class AlienShip extends PathAnimatedImageObject {
   void tick(double delta) {
     super.tick(delta);
 
-    if (Math.random() > 0.99) {
-      if (null != parent) {
-        Bomb bomb = new Bomb(gameContext, pos.x + 16, pos.y + 20, 40);
-        try {
-          parent.addChild(bomb);
-        } catch (final NoSuchMethodException e) {
-          // remove object failed
+    if (lastShoot == 0 || lastShoot < Util.currentTimeMillis() - SHOOT_RATE_MILLIS ) {
+      if (Math.random() > 0.99) {
+        if (null != parent) {
+          Bomb bomb = new Bomb(gameContext, pos.x + 16, pos.y + 20, 40);
+          try {
+            parent.addChild(bomb);
+            } catch (final NoSuchMethodException e) {
+              // remove object failed
+          }
         }
       }
     }
