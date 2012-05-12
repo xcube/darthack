b10bc0c321b94acf4359242555ab20a334740c82
Xabier Burgos <xabier.burgos@gmail.com>
2012-05-12 13:54:04 +0100

    Added bomb collision detection with player ship

--- dart-invaders/dart/Bomb.dart
+++ dart-invaders/dart/Bomb.dart
@@ -9,7 +9,10 @@ class Bomb extends AnimatedImageObject implements Weapon{
 
   void tick(double delta) {
     super.tick(delta);
-
+    if(detectCollision()) {
+      gameContext.getPlayerShip().takeHitFrom(this);
+      remove();
+    }
     if (pos.y > GameScreen.GAME_HEIGHT) {
       remove();
     }
@@ -17,5 +20,9 @@ class Bomb extends AnimatedImageObject implements Weapon{
 
   int getDamage() {
     return damage;
+  }
+
+  bool detectCollision() {
+    return collide(gameContext.getPlayerShip());
   }
 }
