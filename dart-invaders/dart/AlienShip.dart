f578e0cf12451af7ef1853feb5dd4123dd601026
Dave Snowdon <dave@davesnowdon.com>
2012-05-12 11:22:16 +0100

    moved behaviour of AlienShip to new base class

--- dart-invaders/dart/AlienShip.dart
+++ dart-invaders/dart/AlienShip.dart
@@ -1,23 +1,6 @@
-class AlienShip extends Ship {
-  MovingObject mover;
+class AlienShip extends AnimatedImageObject {
 
-  AlienShip(GameContext gameContext, int width, int height, List<Point> wayPoints, double speed) : super(gameContext, 0, 0, width, height, 'img/Space Invaders 2.png') {
-    this.mover = new MovingObject.path(gameContext, width, height, wayPoints, speed);
-    updatePosition();
+  AlienShip(GameContext gameContext, int width, int height, List<Point> wayPoints, double speed, String bitmap) : super(gameContext, width, height, wayPoints, speed, bitmap) {
   }
 
-  /* delegate movment to mover */
-  void tick(double delta) {
-    mover.tick(delta);
-    updatePosition();
-  }
-
-  void updatePosition() {
-    pos.x = mover.pos.x;
-    pos.y = mover.pos.y;
-  }
-
-  void setPathEndBehaviour(int option) {
-    mover.pathEndBehaviour = option;
-  }
 }
